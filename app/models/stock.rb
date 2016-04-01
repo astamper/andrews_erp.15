35f0192b751b0b6fba7aa969f96e2323da5dcff4
Prelang Builder <builder@prelang.com>
2016-04-01 18:35:10 +0000

    Generated model Stock with migration

---
+++ app/models/stock.rb
@@ -0,0 +1,6 @@
+class Stock < ActiveRecord::Base
+  belongs_to :supplier
+  belongs_to :stock_type
+  belongs_to :component
+  belongs_to :order_item
+end
---
+++ spec/factories/stocks.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :stock do
+    initial_quantity 1.5
+    current_quantity 1.5
+    purchase_date "2016-04-01"
+    expiration_date "2016-04-01"
+    purchase_price 1.5
+    batch_number "MyString"
+    supplier nil
+    stock_type nil
+    component nil
+    order_item nil
+    unit "MyString"
+  end
+end
